47eb361ff687bf60d7348c0b00473553ff78dfd3
YardRat <yardrat0117@gmail.com>
2026-01-05 21:58:27 +0800

    ref: unified entrypoint

--- src/lazy/cli.py
+++ src/lazy/cli.py
@@ -1,13 +1,11 @@
 from .printlog.print_log import setup_global_logging
 from .CLI.CLI import app
-import sys
-from pathlib import Path
 
-ROOT_DIR = Path(__file__).resolve().parent.parent.parent
-sys.path.insert(0, str(ROOT_DIR))
 
-# 初始化全局日志记录器
-setup_global_logging()
+def main():
+    setup_global_logging()
+    app()
+
 
 if __name__ == "__main__":
-    app()
+    main()
--- src/lazy/gui.py
+++ src/lazy/gui.py
@@ -1,16 +1,9 @@
 from .printlog.print_log import setup_global_logging
 from .GUI.GUI import app
-import sys
-from pathlib import Path
-
-ROOT_DIR = Path(__file__).resolve().parent.parent.parent
-sys.path.insert(0, str(ROOT_DIR))
-
-# 初始化全局日志记录器
-setup_global_logging()
 
 
 def main():
+    setup_global_logging()
     app.launch()
 
 

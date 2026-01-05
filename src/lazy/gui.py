eb532a84e8e64dbe6ca8036e091fcc3c047a9490
YardRat <yardrat0117@gmail.com>
2026-01-05 21:27:30 +0800

    ref: entrypoint

--- src/lazy/gui.py
+++ src/lazy/gui.py
@@ -1,14 +1,18 @@
+from .printlog.print_log import setup_global_logging
+from .GUI.GUI import app
 import sys
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).resolve().parent.parent.parent
 sys.path.insert(0, str(ROOT_DIR))
 
-from .CLI.CLI import app
-from .printlog.print_log import setup_global_logging
-
 # 初始化全局日志记录器
 setup_global_logging()
 
+
+def main():
+    app.launch()
+
+
 if __name__ == "__main__":
-    app()
+    main()
--- src/lazy_gui_main.py
+++
@@ -1,4 +0,0 @@
-from .Learning_at_ZJU_third_client.main_GUI import main
-
-if __name__ == "__main__":
-    main()
